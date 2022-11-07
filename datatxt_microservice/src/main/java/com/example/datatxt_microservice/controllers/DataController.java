f714e4ffc873ad1a9cbe7f1f429bb485f3973d0f
Daniel <daniel.catalan@usach.cl>
2022-11-07 09:31:34 -0300

    feat: eureka server

--- datatxt_microservice/src/main/java/com/example/datatxt_microservice/controllers/DataController.java
+++ datatxt_microservice/src/main/java/com/example/datatxt_microservice/controllers/DataController.java
@@ -1,5 +1,6 @@
 package com.example.datatxt_microservice.controllers;
 
+import com.example.datatxt_microservice.entities.DataEntity;
 import com.example.datatxt_microservice.services.DataService;
 import com.example.datatxt_microservice.services.UploadService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
 
 
 @RestController
@@ -34,5 +37,29 @@ public class DataController{
         }
     }
 
+    @GetMapping("/get-by-rut")
+    public ResponseEntity<ArrayList<DataEntity>> getByRut(String rut){
+        ArrayList<DataEntity> data = dataService.leerBdByRut(rut);
+        if(data.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(data);
+    }
 
+    @GetMapping("/get-by-rut-and-fecha")
+    public ResponseEntity<ArrayList<DataEntity>> getByRutAndFecha(String rut, String fecha){
+        ArrayList<DataEntity> data = dataService.leerBdByRutAndFecha(rut, fecha);
+        if(data.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(data);
+    }
+
+    @GetMapping("/asistio")
+    public ResponseEntity<Boolean> asistio(String rut, String fecha){
+        if(dataService.asistioEmpleadoDia(rut, fecha)){
+            return ResponseEntity.ok(true);
+        }
+        return ResponseEntity.ok(false);
+    }
 }
