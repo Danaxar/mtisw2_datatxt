6a5ed933470458fc49f01b3cf2f5fb7b565bc621
Daniel <daniel.catalan@usach.cl>
2022-11-11 21:56:18 -0300

    avance

--- datatxt_microservice/scriptData.bat
+++ datatxt_microservice/scriptData.bat
@@ -3,4 +3,4 @@ call git commit -m "avance"
 call git push
 call mvnw clean install -DskipTests
 call docker build -t danaxar/data-microservice .
-call docker build pushdanaxar/data-microservice
+call docker push danaxar/data-microservice
